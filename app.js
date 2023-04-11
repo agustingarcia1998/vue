19a680a249f5b3d612b68623d85131f6b03d8bb6
agustingarcia1998 <agustingarcia280998@gmail.com>
2023-04-11 13:02:25 -0300

    implementacion de funciones

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
 new Vue({
-  el: '#app',
+  el: "#app",
 
-  data () {
-    return{ 
-    name: 'Bitcoin',
-    img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-    changePercent: 10
-  }}
-})
+  data() {
+    return {
+      name: "Bitcoin",
+      symbol: "BTC",
+      img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
+      changePercent: -1,
+
+      color: "f4f4f4",
+
+      price: 8400,
+
+      pricesWithDays: [
+        { day: "Lunes", value: 8400 },
+        { day: "Martes", value: 7900 },
+        { day: "Miércoles", value: 8200 },
+        { day: "Jueves", value: 9000 },
+        { day: "Viernes", value: 9400 },
+        { day: "Sábado", value: 10000 },
+        { day: "Domingo", value: 10200 },
+      ],
+      showPrices: false,
+    };
+  },
+
+  computed: {
+    title() {
+      return `${this.name} - ${this.symbol}`;
+    },
+  },
+
+  watch: {
+    showPrices (newVal, oldVal) {
+      console.log(newVal, oldVal);
+    }
+  },
+
+  methods: {
+    toggleShowPrices() {
+      this.showPrices = !this.showPrices;
+
+      this.color = this.color.split("").reverse().join("");
+    },
+  },
+});
